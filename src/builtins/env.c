e344feae1d231949784b68c1092425fe05847faa
Timothée Rolland <rolland.timothee@gmail.com>
2025-02-06 14:12:44 +0100

    remove unecessary function and updated env_builtin

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -5,29 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: trolland <trolland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 14:18:24 by gschwand          #+#    #+#             */
-/*   Updated: 2025/02/06 12:45:45 by trolland         ###   ########.fr       */
+/*   Created: 2025/02/06 14:12:16 by trolland          #+#    #+#             */
+/*   Updated: 2025/02/06 14:12:18 by trolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+static int	handle_error(const char *msg, const char *arg, int ret)
+{
+	if (arg)
+		ft_dprintf(STDERR_FILENO, "minishell: env: ‘%s’: %s\n", arg, msg);
+	else
+		perror(msg);
+	return (ret);
+}
+
 int	ft_env(char **argv, t_env *env)
 {
 	if (argv[1])
+		return (handle_error("No such file or directory", argv[1], 127));
+	while (env)
 	{
-		ft_dprintf(STDERR_FILENO,
-			"minishell: env: ‘%s’: No such file or directory\n", argv[1]);
-		return (127);
-	}
-	else
-	{
-		while (env)
-		{
-			if (ft_dprintf(STDOUT_FILENO, "%s=%s\n", env->key, env->value) < 0)
-				return (perror("minishell: env: write error"), 125);
-			env = env->next;
-		}
+		if (ft_dprintf(STDOUT_FILENO, "%s=%s\n", env->key, env->value) < 0)
+			return (handle_error("write error", NULL, 125));
+		env = env->next;
 	}
 	return (0);
 }
