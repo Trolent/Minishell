7fc9e4256a93d261a5f4c38b742c8a9db1bc930f
Timothée Rolland <rolland.timothee@gmail.com>
2025-02-06 13:58:17 +0100

    improved pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -5,12 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: trolland <trolland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/05 15:03:00 by gschwand          #+#    #+#             */
-/*   Updated: 2025/02/06 12:53:27 by trolland         ###   ########.fr       */
+/*   Created: 2025/02/06 13:58:03 by trolland          #+#    #+#             */
+/*   Updated: 2025/02/06 13:58:08 by trolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+
+static int	handle_error(const char *msg)
+{
+	perror(msg);
+	return (1);
+}
 
 int	ft_pwd(char **argv, t_env *env)
 {
@@ -20,9 +26,9 @@ int	ft_pwd(char **argv, t_env *env)
 	(void)env;
 	buf = getcwd(NULL, 0);
 	if (buf == NULL)
-		return (perror(PWDERROR), 1);
+		return (handle_error(PWDERROR));
 	if (ft_dprintf(STDOUT_FILENO, "%s\n", buf) == -1)
-		return (perror("minishell: pwd: write error"), free(buf), 1);
+		return (free(buf), handle_error("minishell: pwd: write error"));
 	free(buf);
 	return (0);
 }
